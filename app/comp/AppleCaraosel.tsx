79538ddb91c355786eec538b934ead442acc1e94
YGN <ygntechstartup@gmail.com>
2024-11-18 09:37:25 +0530

    container set

--- app/comp/AppleCaraosel.tsx
+++ app/comp/AppleCaraosel.tsx
@@ -10,7 +10,7 @@ export function Chap () {
   ));
  
   return (
-    <div className="w-full h-full py-20">
+    <div className="w-full h-full py-20 container">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
         Explore Our Chapter
       </h2>
